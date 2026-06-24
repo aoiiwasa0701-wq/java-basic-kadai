0c98260e311abd4b35d7c3217cb61a135f51e7ae
aoiiwasa0701-wq <aoiiwasa0701@outlook.jp>
2026-06-24 14:16:36 +0900

    コミット

--- kadai_015/Car_Chapter15.java
+++ kadai_015/Car_Chapter15.java
@@ -2,41 +2,43 @@ package kadai_015;
 
 public class Car_Chapter15 {
 
-	private int gear = 1;
-	private int speed = 10;
+	private int gear=1;
+	private int speed=10;
 	
 	
-	public Car_Chapter15 (int gear,int speed) {
-		this.gear =gear;
-		this.speed =speed;
+	
 		
-		
-	}
+	
 	
 	public void changeGear(int afterGear) {
 		System.out.println("ギア"+this.gear+"から"+afterGear+"に切り替えました");
 		
+		
 		this.gear =afterGear;
+		
+		switch(this.gear) {
+		case 1 ->this.speed = 10;
+        case 2 -> this.speed = 20;
+        case 3 -> this.speed = 30;
+        case 4 -> this.speed = 40;
+        case 5 -> this.speed = 50;
+        default -> this.speed = 10;
+    }
 	}
+	
 		
 		
 	public void run() {
 		
-		switch(this.gear) {
-			case 1 ->this.speed = 10;
-	        case 2 -> this.speed = 20;
-	        case 3 -> this.speed = 30;
-	        case 4 -> this.speed = 40;
-	        case 5 -> this.speed = 50;
-	        default -> this.speed = 10;
-	    }
+		
+			
 
 	    System.out.println("速度は時速" + this.speed + "kmです");
-	}
+		}
 	}
 		
 	
-	
-	
+
+
 	
 
